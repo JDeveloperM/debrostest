a07d6bc37edfa10eda41c705f221fcd33e7b883f
JDeveloperM <digitalvideomovies@gmail.com>
2025-06-27 12:37:25 +0300

    refactor to decrease size

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,8 @@ import Loading from "./loading";
 
 import "bootstrap/scss/bootstrap.scss";
 // ========= Plugins CSS START =========
-import "../public/css/plugins/feature.css";
-import "../public/css/plugins/fontawesome-all.min.css";
+import "../public/css/plugins/feather-cdn.css";
+import "../public/css/plugins/fontawesome-cdn.css";
 import "../public/css/plugins/animation.css";
 import "../node_modules/sal.js/dist/sal.css";
 import "slick-carousel/slick/slick.css";
